e09b0856521023677812af88f8bd1deb2088809e
aldi7id <aldi7.indo@gmail.com>
2022-03-16 15:40:19 +0700

    make a view bidding

--- app/src/main/java/com/ajgroup/challengechapter3binaracademy/MainActivity.kt
+++ app/src/main/java/com/ajgroup/challengechapter3binaracademy/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ajgroup.challengechapter3binaracademy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ajgroup.challengechapter3binaracademy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
