07b6401b74895913bc9368f3ad54d65a0bdeea21
aldi7id <aldi7.indo@gmail.com>
2022-03-17 14:07:07 +0700

    binding firstfragment

--- app/src/main/java/com/ajgroup/challengechapter3binaracademy/FirstFragment.kt
+++ app/src/main/java/com/ajgroup/challengechapter3binaracademy/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.ajgroup.challengechapter3binaracademy.databinding.FragmentFirstBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FirstFragment : Fragment() {
+    private lateinit var binding:FragmentFirstBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,8 +37,17 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
+        binding.goToScreenTwo.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
     }
+
 
     companion object {
         /**
